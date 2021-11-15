813b74d689fdba8a66e8057104b49b59c45c4977
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-15 20:35:30 -0300

    Auditing data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (NAME) VALUES ('Books');
+INSERT INTO tb_category (NAME,CREATED_AT) VALUES ('Books',now());
 INSERT INTO tb_category (NAME) VALUES ('Electronics');
-INSERT INTO tb_category (NAME) VALUES ('Computers');
+INSERT INTO tb_category (NAME) VALUES ('Computers'); 

e8b797af48eae1fa48bbc27fc7e857d26e740e1d
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-16 10:36:33 -0300

    Pagination

--- backend/src/main/java/com/lucasdev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/lucasdev/dscatalog/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.lucasdev.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +25,9 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = categoryRepository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> pagelist = categoryRepository.findAll(pageRequest);
+		return pagelist.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)

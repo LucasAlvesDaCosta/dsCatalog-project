5007ab1327dcb78dedd20424eafc3a1a3da8e0b6
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2021-11-17 11:21:50 -0300

    Product insert, update, delete

--- backend/src/main/java/com/lucasdev/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/lucasdev/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.lucasdev.dscatalog.dto.CategoryDTO;
 import com.lucasdev.dscatalog.dto.ProductDTO;
+import com.lucasdev.dscatalog.entities.Category;
 import com.lucasdev.dscatalog.entities.Product;
+import com.lucasdev.dscatalog.repositories.CategoryRepository;
 import com.lucasdev.dscatalog.repositories.ProductRepository;
 import com.lucasdev.dscatalog.services.exceptions.DataBaseException;
 import com.lucasdev.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -40,7 +46,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);// method to avoid code repetition
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
@@ -49,7 +55,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getOne(id);
-			//entity.setName(dto.getName());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		}
@@ -59,7 +65,8 @@ public class ProductService {
 		
 	}
      
-	//não utilizar o @transactional para receber a excessão do banco
+
+	//do not use @transactional = receive database exception
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
@@ -69,6 +76,21 @@ public class ProductService {
 		}
 		catch (DataIntegrityViolationException e) {
 			throw new DataBaseException("Integrity violation");
+		}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
 		}
 	}
 }

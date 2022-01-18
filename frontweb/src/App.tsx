32159ecd89015165f2be1ef064b52439c7c70efc
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2022-01-18 11:08:49 -0300

    Navbar styling, Lock homolog AWS

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
 import './assets/styles/custom.scss';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>
+    </>
   );
 }
 

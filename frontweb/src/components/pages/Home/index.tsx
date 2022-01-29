3d5d8a65a14a93d2caf4695f444548356b716d31
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2022-01-29 14:32:36 -0300

    Props in button - home page

--- frontweb/src/components/pages/Home/index.tsx
+++ frontweb/src/components/pages/Home/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
           </div>
           <div>
             <Link to="/products">
-              <ButtonIcon />
+              <ButtonIcon text="Incie agora a sua busca"/>
             </Link>
           </div>
         </div>

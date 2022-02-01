cf06df2b76a7d25ec15a011835d62de01235e9aa
LucasAlvesDaCosta <lucasalves16.la@gmail.com>
2022-02-01 11:23:28 -0300

    Admin Navbar layout mobile

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,6 +1,14 @@
-const Admin = () =>{
-    return(
-        <h1>Página do Admin...</h1>
-    );
-}
+import Navbar from './Navbar';
+import './styles.css';
+
+const Admin = () => {
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+        <h1>Conteúdo</h1>
+      </div>
+    </div>
+  );
+};
 export default Admin;
